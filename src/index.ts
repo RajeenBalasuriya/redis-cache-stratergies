0529e1d353bcd5fd6c0e203dd0c39ea2cfa190bf
RajeenBalasuriya <rajeenbalasuriya1@gmail.com>
2025-06-19 02:30:07 +0530

    Initialize database connection

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { Request, Response } from "express";
+import { appDataSource } from "./datasource";
 
 const app = express();
 app.use(express.json());
@@ -7,9 +8,18 @@ app.use(express.json());
 //allow application to listen on a port
 const PORT = process.env.PORT || 3001;
 
+//eshtablish data base connection
+appDataSource.initialize().then(()=>{
+    console.log("Data source has been intialized")
+}).catch((err)=>{
+    console.log("Error during data source intialization")
+    console.log(err)
+})
+
 app.listen(PORT, () => {
   console.log(`Application is listening on ${PORT}`);
 });
+
 //route to check express applicaation
 app.get("/health", async (req: Request, res: Response) => {
   res.send("I AM HEALTHY :)");
