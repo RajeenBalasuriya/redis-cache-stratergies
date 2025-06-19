b778962b9cda926f3571c15e76d2135a7e1be0da
RajeenBalasuriya <rajeenbalasuriya1@gmail.com>
2025-06-19 12:55:18 +0530

    put configurations onto .env file

--- src/datasource.ts
+++ src/datasource.ts
@@ -1,12 +1,13 @@
 import { DataSource } from "typeorm";
 
 export const appDataSource = new DataSource({
-    type: "mysql",
-    host: "localhost",
-    port: 3306,
-    username: "root",
-    database: "sakila",
-    entities: ["src/entities/*.js"],
-    logging: true,
-    synchronize: false,
+  type: process.env.DB_TYPE as any, // 'mysql'
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || '3306'),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  synchronize: process.env.DB_SYNCHRONIZE === 'true',
+  logging: process.env.DB_LOGGING === 'true',
+  entities: ['src/entities/*.ts'], // or use explicit list
 });
